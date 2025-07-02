65b085e1a252dd73ba3034bb5aef2963fef15bb8
kemasmalfath <alfathisk1453@gmail.com>
2025-07-02 09:10:33 +0700

    Tambah index dan style

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include 'header.php'; ?>
+
+    <main>
+        <h2>Selamat Datang di Aplikasi BebasTema Kami!</h2>
+        <p>Ini adalah halaman beranda aplikasi sederhana dengan fitur pilihan adantara light mode ataudark mode.</p>
+        <p>Coba klik tombol "di kanan atas" di kanan atas untuk melihat perubahannya.</p>
+    </main>
+
+<?php include 'footer.php'; ?>
