752da2be87da5138cc56cd0ad9b605cb862c1c81
Kemas_M_Alfath <153353493+kemasmalfath@users.noreply.github.com>
2025-07-15 20:14:49 +0700

    Update about.php

--- about.php
+++ about.php
@@ -2,7 +2,7 @@
 
     <main>
         <h2>Tentang Kami</h2>
-        <p>Aplikasi ini dibuat sebagai contoh sederhana untuk menunjukkan implementasi fitur dark mode.</p>
+        <p>Aplikasi ini dibuat sebagai contoh sederhana untuk menunjukkan implementasi fitur dark mode</p>
         <p>Kami harap contoh ini dapat membantu Anda memahami konsepnya</p>
     </main>
 
