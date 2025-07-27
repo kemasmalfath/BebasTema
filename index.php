2e983b5677fedc3d95b33aa6500bd5a6739562bd
Kemas_M_Alfath <153353493+kemasmalfath@users.noreply.github.com>
2025-07-27 09:41:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <main>
         <h2>Selamat Datang di Aplikasi BebasTema Kami!</h2>
         <p>Ini adalah halaman beranda aplikasi sederhana dengan fitur pilihan adantara light mode ataudark mode.</p>
-        <p>Coba klik tombol "di kanan atas" di kanan atas untuk melihat perubahannya.</p>
+        <p>Coba klik tombol "di kanan atas" di kanan atas untuk melihat perubahannya</p>
     </main>
 
 <?php include 'footer.php'; ?>
