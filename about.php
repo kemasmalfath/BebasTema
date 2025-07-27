e11253c46b7ba8232f6b36685981e817b83c84f4
Kemas_M_Alfath <153353493+kemasmalfath@users.noreply.github.com>
2025-07-27 09:40:51 +0700

    Update about.php

--- about.php
+++ about.php
@@ -3,7 +3,7 @@
     <main>
         <h2>Tentang Kami< /h2>
         <p>Aplikasi ini dibuat sebagai contoh sederhana untuk menunjukkan implementasi fitur dark mode.</p>
-        <p>Kami harap contoh ini dapat membantu Anda memahami konsepnya.</p>
+        <p>Kami harap contoh ini dapat membantu Anda memahami konsepnya</p>
     </main>
 
 <?php include 'footer.php'; ?>
